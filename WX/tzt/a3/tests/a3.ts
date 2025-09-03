a6d517b4655742c30a363d3bf495c3aba3ffa62b
kachraz <110726496+kachraz@users.noreply.github.com>
2025-09-03 01:17:19 +0000

    🫦

--- WX/tzt/a3/tests/a3.ts
+++ WX/tzt/a3/tests/a3.ts
@@ -1,16 +1,17 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { A3 } from "../target/types/a3";
+import * as anchor from "@coral-xyz/anchor"
+import { Program } from "@coral-xyz/anchor"
+import { A3 } from "../target/types/a3"
 
 describe("a3", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  anchor.setProvider(anchor.AnchorProvider.env())
 
-  const program = anchor.workspace.a3 as Program<A3>;
+  const program = anchor.workspace.a3 as Program<A3>
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+    const tx = await program.methods.initialize().rpc()
+    console.log("Your transaction signature", tx)
+    console.log("Fuck all Night", tx)
+  })
+})
